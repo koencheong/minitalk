ba29cc8585825aaacc3bf25a9d985b14d5f2f188
Ko en Cheong <kcheong@u90z03s03.42kl.edu.my>
2023-04-12 22:33:10 +0800

    final

--- client.c
+++ client.c
@@ -14,8 +14,6 @@
 #include <stdlib.h>
 #include "libft/libft.h"
 
-void	bit_transfer(int bit, char *message, int pid, int byte);
-
 // client
 
 void	sig_handler2(int sig)
@@ -25,38 +23,41 @@ void	sig_handler2(int sig)
 	exit(0);
 }
 
+void	bit_transfer(char *message, int pid)
+{
+	int byte;
+	int	bit;
+
+	byte = 0;
+	while (byte <= (int)(ft_strlen(message)))
+	{
+		bit = 8;
+		while (bit > 0)
+		{
+			usleep(150);
+			if ((message[byte] >> --bit) & 1)
+				kill(pid, SIGUSR1);
+			else
+				kill(pid, SIGUSR2);
+			usleep(150);
+		}
+		byte++;
+	}
+}
+
 int	main(int argc, char *argv[])
 {
-	int		bit;
-	int		byte;
 	int		pid;
 	char	*message;
 
-	bit = 0;
 	signal(SIGUSR2, sig_handler2);
 	if (argc != 3)
 	{
 		ft_printf("ERROR: Invalid arguments passed!\n");
 		exit(EXIT_FAILURE);
 	}
-	byte = 0;
 	pid = ft_atoi(argv[1]);
 	message = argv[2];
-	while (byte <= (int)(ft_strlen(message)))
-		bit_transfer(bit, message, pid, byte);
+	bit_transfer(message, pid);
 }
 
-void	bit_transfer(int bit, char *message, int pid, int byte)
-{
-	bit = 8;
-	while (bit > 0)
-	{
-		usleep(150);
-		if ((message[byte] >> --bit) & 1)
-			kill(pid, SIGUSR1);
-		else
-			kill(pid, SIGUSR2);
-		usleep(150);
-	}
-	byte++;
-}
